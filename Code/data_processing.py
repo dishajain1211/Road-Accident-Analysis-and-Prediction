05d2a460614de71e81af2eb76e207fd0b7217ecb
adityamagarde <adityamagarde26@gmail.com>
2018-11-09 00:59:46 +0530

    Started writing the code for Data preprocessing module

---
+++ Code/data_processing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 21:56:47 2018
+
+@author: adityamagarde
+"""
+
+import numpy as np
+import pandas as pd
+
+
+dataset = pd.read_csv(r"/home/adityamagarde/Documents/Projects/RAP-ter/Datasets/1.6/1-6m-accidents-traffic-flow-over-16-years/accidents_2012_to_2014.csv")
