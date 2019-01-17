0d749b7a13c2510d0e1d4130479a955f9ac371cf
dishajain1211 <dishajain1211@gmail.com>
2019-01-18 01:03:21 +0530

    Update and rename dj_utils.py to generate_roa_name.py

---
+++ Code/generate_roa_name.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 22:58:15 2019
+@author: Disha Jain
+"""
+
+import pandas as pd
+
+dataset = pd.read_csv(Path)                                     #read dataset from path
+roadname = []   
+
+print(dataset)
+
+def road_class(classname):                                      #convert class number to class name
+    if classname == 1:
+        return 'M'
+    elif classname == 2:
+        return 'A(M)'
+    elif classname == 3:
+        return 'A'
+    elif classname == 4:
+        return 'B'
+    elif classname == 5:
+        return 'C'
+    elif classname == 6:
+        return 'U'
+
+def road_name(dataset):                                         #generate road name
+    for index, row in dataset.iterrows():
+        road_class_number = road_class(row['1st_Road_Class'])   #read class number
+        road_number = row['1st_Road_Number']                    #read road number
+        x = road_class_number + str(road_number)                #create road name
+        roadname.append(x)
+    return roadname
+
+print(road_name(dataset))
