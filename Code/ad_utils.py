de54e7006176e0570c018688562642332cef740e
Aditya Magarde <adityamagarde26@gmail.com>
2019-01-29 14:00:38 +0530

    Added road_stats, average_road_stats

--- Code/ad_utils.py
+++ Code/ad_utils.py
@@ -32,7 +32,7 @@ def stats_set_creation(stats, coloumnName):
     return roadsAccidentsSet
 
 
-#Function that takes the roadAccidentSet(Roads and respective number of Accidents on that road) and roadName (Road on which an accident occured) as input and returns updated roadAccidentSet
+#Function that takes the roadAccidentSet(Roads and respective number of Accidents on that road) and roadName (Road on which an accident occured) as input and returns updated roadAccidentSet and a set of accidents on unassignedRoads
 def accident_flows(roadAccidentsSet, roadName):
     unassignedRoads = []
     for road in roadName:
@@ -49,7 +49,8 @@ def accident_flows(roadAccidentsSet, roadName):
 
 
 #Helper function for accidents_per_classes() to calculate the number of accidents on road types
-def helper_for_acp(dictionaryOfClasses, array):
+#pass dictionary and roadAccidentSet and unassignedRoads one by one
+def helper_for_apc(dictionaryOfClasses, array):
     for road in array:
         if road[-1] == ')':
             dictionaryOfClasses['A(M)'] = dictionaryOfClasses['A(M)'] + 1
@@ -65,20 +66,74 @@ def helper_for_acp(dictionaryOfClasses, array):
             dictionaryOfClasses['U'] = dictionaryOfClasses['U'] + 1
     
     return dictionaryOfClasses
+
+
     
     
 #Function to calculate the total accidents in the road cateogories A(M), A, B, C, U, M
 def accidents_per_class(roadAccidentsSet, unassignedRoads):
     dictionaryOfClasses = {'A(M)': 0, 'A':0, 'B':0, 'C':0, 'M':0, 'U':0}                    #Dictionary comprising of the count of all classes of roads
     
-    dictionaryOfClasses = helper_for_acp(dictionaryOfClasses, roadAccidentsSet[1])
-    dictionaryOfClasses = helper_for_acp(dictionaryOfClasses, unassignedRoads)
+    dictionaryOfClasses = helper_for_apc(dictionaryOfClasses, roadAccidentsSet[1])
+    dictionaryOfClasses = helper_for_apc(dictionaryOfClasses, unassignedRoads)
     
     return dictionaryOfClasses
 
 
 
+#Function that calculates the traffic stats per road type
+#Takes as input roadAccidentSet that has roads which are uniquely identified in the traffic statistics dataset
+def road_stats(roadAccidentSet, stats):
+    cols = roadAccidentSet.shape[1]
+    
+    #The traffic analysis dataset consists of the following coloumns that we'll consider
+    #   FdPC – AADF for pedal cycles.
+    #   Fd2WMV – AADF for two-wheeled motor vehicles.
+    #   FdCar - AADF for Cars and Taxis.
+    #   FdBUS – AADF for Buses and Coaches
+    #   FdLGV – AADF for LGVs.
+    #   FdHGV – AADF for all HGVs.
+    #   FdAll_MV – AADF for all motor vehicles.
+    #   count - Number of times the road appeared
+    numZeros = 8
+    shape = (numZeros, cols)
+    
+    zerosArray = np.zeros(shape)
+    alteredRoadAccident = np.concatenate((roadAccidentSet, zerosArray), axis = 0).T
+    
+    for index, rows in stats.iterrows():
+        road = rows['Road']
 
-#Function that 
+        pos, _ = np.where(alteredRoadAccident == road)          #pos[0] here contains the index at which the corresponding road is stored in alteredRoadAccident
+        
+        #Updating the alteredRoadAccident at pos[0] with values obtained from row
+        alteredRoadAccident[pos[0], 2] = alteredRoadAccident[pos[0], 2] + rows['FdPC']
+        alteredRoadAccident[pos[0], 3] = alteredRoadAccident[pos[0], 3] + rows['Fd2WMV']
+        alteredRoadAccident[pos[0], 4] = alteredRoadAccident[pos[0], 4] + rows['FdCar']
+        alteredRoadAccident[pos[0], 5] = alteredRoadAccident[pos[0], 5] + rows['FdBUS']
+        alteredRoadAccident[pos[0], 6] = alteredRoadAccident[pos[0], 6] + rows['FdLGV']
+        alteredRoadAccident[pos[0], 7] = alteredRoadAccident[pos[0], 7] + rows['FdHGV']
+        alteredRoadAccident[pos[0], 8] = alteredRoadAccident[pos[0], 8] + rows['FdAll_MV']
+        alteredRoadAccident[pos[0], 9] = alteredRoadAccident[pos[0], 9] + 1
+        
+    return alteredRoadAccident
+
+
+#This function takes the alteredRoadAccident array and fetches the value of coount and divides all values in that row by count to obtain the average
+def average_road_stats(alteredRoadAccident):
+    for i in range(len(alteredRoadAccident)):
+        count = alteredRoadAccident[i, 9]                   #Count obtained from that row
+        if count != 0:
+            alteredRoadAccident[i, 2:] = alteredRoadAccident[i, 2:]//count
+            
+    
+    return alteredRoadAccident
+
+    
+    
+    
+    
+    
+    
 #stats2017 = major_minor_stat_creation('F:\**\AADF-data-major-roads.csv', 'F:\**\AADF-data-minor-roads.csv', 2017)
 #setOfRoads2017 = stats_set_creation(stats2017, 'Road')
--- Code/main.py
+++ Code/main.py
@@ -6,5 +6,31 @@ Created on Thu Jan 17 23:50:53 2019
 """
 
 import ad_utils
+import generate_road_name
+    
+import numpy as np
+import pandas as pd
+
+stats = ad_utils.major_minor_stat_creation(r'F:\Files\MajorProject\RAP-ter-master\Data\AADF-data-major-roads.csv', r'F:\Files\MajorProject\RAP-ter-master\Data\AADF-data-minor-roads.csv', 2017)
+roadAccidentsSet = ad_utils.stats_set_creation(stats, 'Road')
+roadName = generate_road_name.road_name()
+
+roadAccidentsSetUpdated, unassignedRoads = ad_utils.accident_flows(roadAccidentsSet, roadName)
+setOfUnassignedRoads = set(unassignedRoads)                                                 #Creating a set of unassigned roads
+
+alteredRoadAccident = ad_utils.road_stats(roadAccidentsSet, stats)
 
 
+##Function that takes the roadAccidentSet(Roads and respective number of Accidents on that road) and roadName (Road on which an accident occured) as input and returns updated roadAccidentSet
+#def accident_flows(roadAccidentsSet, roadName):
+#    unassignedRoads = []
+#    for road in roadName:
+#        if road in roadAccidentsSet:
+#            tempLocation = np.where(roadAccidentsSet == road)                               #The location where the road is in roadAccidentSet
+#            indexToBeInc = tempLocation[1][0]                                               #The index of that location that need to be incremented
+#            roadAccidentsSet[1][indexToBeInc] = roadAccidentsSet[1][indexToBeInc] + 1       #Incrementing the count on that index
+#        else:
+#            unassignedRoads.append(road)
+#            
+#        unassignedRoads = np.array(unassignedRoads)
+#    return roadAccidentsSet, unassignedRoads
